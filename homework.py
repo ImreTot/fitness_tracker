ab8fdfb6e6d977f1403f6c18995a7355d3dc6524
Roman Kiiashko <saycoron@gmail.com>
2022-12-25 14:39:57 +0300

    Fix last warning after second code review.

--- homework.py
+++ homework.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
-        ('WL', [9000, 1, 75, 180]),
+        ('WLK', [9000, 1, 75, 180]),
     ]
 
     for workout_type, data in packages:
